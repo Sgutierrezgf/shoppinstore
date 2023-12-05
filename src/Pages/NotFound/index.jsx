a1ec1a8eb81dcf92e8d84eed239d3cebf5e8f352
sgutierrezg <segut10@gmail.com>
2023-12-05 14:24:39 -0500

    upload poryect shoppingstore

---
+++ src/Pages/NotFound/index.jsx
@@ -0,0 +1,6 @@
+import Layout from "../../components/Layout";
+function NotFound() {
+  return <Layout>NotFound</Layout>;
+}
+
+export default NotFound;
---
+++ src/Pages/SignIn/index.jsx
@@ -0,0 +1,6 @@
+import Layout from "../../components/Layout";
+function SignIn() {
+  return <Layout>SignIn</Layout>;
+}
+
+export default SignIn;
